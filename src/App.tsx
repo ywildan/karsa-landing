8e25bc2e93ebeb6e5ff0dd0cdac884f79db1c059
Yusuf Wildan Affandi <yuwiaffa@gmail.com>
2026-09-18 13:25:17 +0700

    fix: reset scroll position on route change
    - Add ScrollToTop component using useLocation
    - Scroll to top instantly when pathname changes
    - Fixes issue where landing footer link to /privacy kept scroll position

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import PrivacyPage from "./pages/PrivacyPage";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/privacy" element={<PrivacyPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+      </Routes>
+    </>
   );
 }
